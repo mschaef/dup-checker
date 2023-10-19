5efd6947037053e3261625cb0f79cde4a2bcce2c
Mike Schaeffer <mike@mschaef.com>
2023-10-19 06:08:11 -0400

    Scan a directory returning summary information on files contained within.

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "Duplicate file checking tool"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clj-commons/digest "1.4.100"]] 
+
   :repl-options {:init-ns dup-checker.core}
 
   :main dup-checker.core
--- src/dup_checker/core.clj
+++ src/dup_checker/core.clj
@@ -1,5 +1,17 @@
-(ns dup-checker.core)
+(ns dup-checker.core
+  (:require [clj-commons.digest :as digest]))
+
+(defn- file-info [ f ]
+  {:name (.getAbsolutePath f)
+   :size (.length f)
+   :md5-digest (digest/md5 f)
+   :sha256-digest (digest/sha256 f)})
+
+(defn- list-files []
+  (let [root (clojure.java.io/file ".")]
+    (doseq [f (filter #(.isFile %) (file-seq root))]
+      (println (file-info f)))))
 
 (defn -main [& args]
-  (println "args " args)
+  (list-files)
   (println "end run."))
