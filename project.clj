0c28bc103c92cd62b159115f292bbd91067e4729
Mike Schaeffer <mike@mschaef.com>
2023-11-10 06:41:06 -0500

    Add retry logic to the digest calculation to tolerate intermittant I/O errors.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [listora/again "1.0.0"]
                  [org.clj-commons/digest "1.4.100"]
                  [com.mschaef/sql-file "0.4.8"]
                  ;; TODO: This brings in an awful lot for a CLI app.
