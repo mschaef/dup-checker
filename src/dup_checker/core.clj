d20c714b6893036e59a8bd91563b01f6d49fac25
Mike Schaeffer <mike@mschaef.com>
2023-11-13 16:54:14 -0500

    Add a command to query a S3 bucket for a directory catalog.

--- src/dup_checker/core.clj
+++ src/dup_checker/core.clj
@@ -9,10 +9,16 @@
             [playbook.config :as config]
             [taoensso.timbre :as log]
             [clojure.java.jdbc :as jdbc]
+            [clojure-ini.core :as ini]
             [again.core :as again]))
 
 (defn- fail [ message ]
   (throw (RuntimeException. message)))
+
+(defn- s3-client []
+  (-> (software.amazon.awssdk.services.s3.S3Client/builder)
+      (.region software.amazon.awssdk.regions.Region/US_EAST_1)
+      (.build)))
 
 (defn- get-file-extension [ f ]
   (let [name (.getName f)
@@ -34,11 +40,13 @@
 (def retry-policy [ 0 5000 10000 15000 ])
 
 (defn- compute-file-digests [ file-info ]
-  (merge file-info
+  (if (:md5-digest file-info)
+    file-info
+    (merge file-info
          ;;; MD5 only, because that's all the S3 API supports.
-         {:md5-digest (again/with-retries retry-policy
+           {:md5-digest (again/with-retries retry-policy
                         ;;; Retry to accomodate potential I/O errors.
-                        (digest/md5 (:file file-info)))}))
+                          (digest/md5 (:file file-info)))})))
 
 (defn- file-cataloged? [ db-conn catalog-id file-info ]
   (> (query-scalar db-conn
@@ -100,40 +108,52 @@
 (defn- current-hostname []
   (.getCanonicalHostName (java.net.InetAddress/getLocalHost)))
 
-(defn- create-catalog [ db-conn catalog-name root-path ]
+(defn- find-catalog-type-id [ db-conn catalog-type ]
+  ;; TODO: query-scaler-required
+  (query-scalar db-conn
+                [(str "SELECT catalog_type_id"
+                      "  FROM catalog_type"
+                      " WHERE catalog_type.catalog_type = ?")
+                 catalog-type]))
+
+(defn- create-catalog [ db-conn catalog-name root-path catalog-type]
   (:catalog_id (first
                 (jdbc/insert! db-conn
                               :catalog
                               {:name catalog-name
+                               :catalog_type_id (find-catalog-type-id db-conn catalog-type)
                                :created_on (java.util.Date.)
                                :updated_on (java.util.Date.)
                                :root_path root-path
                                :hostname (current-hostname)}))))
 
-(defn- ensure-catalog [ db-conn catalog-name root-path ]
+(defn- ensure-catalog [ db-conn catalog-name root-path catalog-type ]
   (if-let [ existing-catalog-id (get-catalog-id db-conn catalog-name ) ]
     (update-catalog-date db-conn existing-catalog-id)
-    (create-catalog db-conn catalog-name root-path)))
+    (create-catalog db-conn catalog-name root-path catalog-type)))
 
 (defn- cmd-catalog-fs-files [ db-conn catalog-name root-path ]
-  (let [catalog-id (ensure-catalog db-conn catalog-name root-path)
+  (let [catalog-id (ensure-catalog db-conn catalog-name root-path "fs")
         root (clojure.java.io/file root-path)
         catalog-files (get-catalog-files db-conn catalog-id)]
     (doseq [f (filter #(.isFile %) (file-seq root))]
       (catalog-file db-conn catalog-files catalog-id (file-info root f)))))
 
-(defn- cmd-list-catalogs [ db-conn]
+(defn- cmd-list-catalogs [ db-conn ]
   (pprint/print-table
    (map (fn [ catalog-rec ]
           {:n (:n catalog-rec)
            :name (:name catalog-rec)
+           :root-path (:root_path catalog-rec)
+           :catalog-type (:catalog_type catalog-rec)
            :size (:size catalog-rec)
            :updated-on (:updated_on catalog-rec)})
         (query-all db-conn
-                   [(str "SELECT catalog.name, catalog.updated_on, count(file_id) as n, sum(file.size) as size"
-                         "  FROM catalog, file"
+                   [(str "SELECT catalog.name, catalog.root_path, catalog.updated_on, count(file_id) as n, sum(file.size) as size, catalog_type.catalog_type"
+                         "  FROM catalog, file, catalog_type"
                          " WHERE catalog.catalog_id = file.catalog_id"
-                         " GROUP BY catalog.name, catalog.updated_on"
+                         "   AND catalog.catalog_type_id = catalog_type.catalog_type_id"
+                         " GROUP BY catalog.name, catalog.updated_on, catalog_type, catalog.root_path"
                          " ORDER BY name")]))))
 
 (defn- cmd-list-catalog-files [ db-conn catalog-name ]
@@ -165,16 +185,53 @@
 
     (println "n=" (count result-set))))
 
+(defn- s3-list-bucket-paged [ s3 bucket-name ]
+  (letfn [(s3-list-objects [ cont-token ]
+            (log/info "s3 listing with token: " cont-token)
+            (let [ resp (.listObjectsV2 s3 (-> (software.amazon.awssdk.services.s3.model.ListObjectsV2Request/builder)
+                                               (.bucket bucket-name)
+                                               (.continuationToken cont-token)
+                                               (.build)))]
+              (log/info "RESP" resp)
+              (if (.isTruncated resp)
+                (lazy-seq (concat (.contents resp)
+                                  (s3-list-objects (.nextContinuationToken resp))))
+                (.contents resp))))]
+
+    (s3-list-objects nil)))
+
+(defn- s3-blob-info [ f ]
+  {:full-path (.key f)
+   :extension (get-file-extension (java.io.File. (.key f)))
+   :last-modified-on (.lastModified f)
+   :name (.key f)
+   :size (.size f)
+   :md5-digest (.eTag f)})
+
+(defn- cmd-catalog-s3-files [ db-conn catalog-name bucket-name ]
+  (let [catalog-id (ensure-catalog db-conn catalog-name bucket-name "s3")
+        catalog-files (get-catalog-files db-conn catalog-id)]
+
+    (doseq [f (s3-list-bucket-paged (s3-client) bucket-name)]
+      (catalog-file db-conn catalog-files catalog-id (s3-blob-info f)))))
+
+(defn cmd-s3-list-bucket [ db-conn bucket-name ]
+  (doseq [ bucket (s3-list-bucket-paged (s3-client) bucket-name)]
+    (pprint/pprint bucket)))
+
 (defn- dispatch-subcommand [ db-conn args ]
   (if (= (count args) 0)
     (fail "Insufficient arguments.")
     (let [ [ subcommand & args ] args ]
 
       (case subcommand
+        "s3cat" (cmd-catalog-s3-files db-conn
+                                      (or (second args) "default")
+                                      (first args))
         "lsc" (cmd-list-catalogs db-conn)
         "catalog" (cmd-catalog-fs-files db-conn
-                                    (or (second args) "default")
-                                    (or (first args) "."))
+                                        (or (second args) "default")
+                                        (or (first args) "."))
         "list" (cmd-list-catalog-files db-conn (or (first args) "default"))
         "list-dups" (cmd-list-dups db-conn)
         (fail "Unknown subcommand")))))
@@ -198,7 +255,7 @@
   {:name (or (config-property "db.subname")
              (get-in config [:db :subname] "dup-checker"))
    :schema-path [ "sql/" ]
-   :schemas [[ "dup-checker" 0 ]]})
+   :schemas [[ "dup-checker" 1 ]]})
 
 (defn -main [& args] ;; TODO: Does playbook need a standard main? Or wrapper?
   (app-main
