1f1218920108154557829e1a92359359e6a5e16a
Mike Schaeffer <mike@mschaef.com>
2024-01-09 13:36:42 -0500

    Break out the description commands into their own subcommand.

--- src/dup_checker/core.clj
+++ src/dup_checker/core.clj
@@ -11,70 +11,17 @@
             [taoensso.timbre :as log]
             [clojure.java.jdbc :as jdbc]
             [dup-checker.catalog :as catalog]
+            [dup-checker.describe :as describe]
             [dup-checker.fs :as fs]
             [dup-checker.gphoto :as gphoto]
             [dup-checker.s3 :as s3]))
-
-(defn- all-filenames-by-digest [ ]
-  (into {} (map (fn [ value ]
-                  [(:md5_digest value) (:name value)])
-                (query-all (sfm/db)
-                           [(str "SELECT md5_digest, name"
-                                 "  FROM file")]))))
-
-(defn- cmd-list-dups
-  "List all duplicate files by MD5 digest."
-  [ ]
-
-  (let [ md5-to-filename (all-filenames-by-digest)]
-    (table
-     (map (fn [ file-rec ]
-            {:md5-digest (:md5_digest file-rec)
-             :count (:count file-rec)
-             :name (md5-to-filename (:md5_digest file-rec))})
-          (query-all (sfm/db)
-                     [(str "SELECT * FROM ("
-                           "   SELECT md5_digest, count(md5_digest) as count"
-                           "     FROM file"
-                           "    GROUP BY md5_digest)"
-                           " WHERE count > 1"
-                           " ORDER BY count")])))))
-
-(defn- cmd-describe-digest
-  "List every instance of a file with a given MD5 digest."
-  [ md5-digest ]
-  (table
-   [:catalog_name :file_name :last_modified_on :size]
-   (query-all (sfm/db)
-              [(str "SELECT file.name as file_name, catalog.name as catalog_name, file.size, file.last_modified_on"
-                    "  FROM file, catalog"
-                    " WHERE md5_digest=?"
-                    "  AND file.catalog_id=catalog.catalog_id"
-                    " ORDER BY catalog_name")
-               md5-digest])))
-
-(defn- cmd-describe-filename
-  "List every instance of a file with the given text in its filename."
-
-  [ filename-segment ]
-  (table
-   [:catalog_name :file_name :last_modified_on :size :md5_digest]
-   (query-all (sfm/db)
-              [(str "SELECT file.name as file_name, catalog.name as catalog_name, file.size, file.last_modified_on, md5_digest"
-                    "  FROM file, catalog"
-                    " WHERE INSTR(file.name, ?) > 0"
-                    "  AND file.catalog_id=catalog.catalog_id"
-                    " ORDER BY catalog_name")
-               filename-segment])))
 
 (def subcommands
   {"s3" s3/subcommands
    "catalog" catalog/subcommands
    "gphoto" gphoto/subcommands
    "fs" fs/subcommands
-   "describe-digest" #'cmd-describe-digest
-   "describe-filename" #'cmd-describe-filename
-   "list-dups" #'cmd-list-dups})
+   "describe" describe/subcommands})
 
 (defn- display-help [ cmd-map ]
   (println "Valid Commands:")
---
+++ src/dup_checker/describe.clj
@@ -0,0 +1,63 @@
+(ns dup-checker.describe
+  (:use playbook.core
+        sql-file.sql-util
+        dup-checker.util)
+  (:require [sql-file.middleware :as sfm]
+            [taoensso.timbre :as log]))
+
+(defn- all-filenames-by-digest [ ]
+  (into {} (map (fn [ value ]
+                  [(:md5_digest value) (:name value)])
+                (query-all (sfm/db)
+                           [(str "SELECT md5_digest, name"
+                                 "  FROM file")]))))
+
+(defn- cmd-describe-duplicates
+  "List all duplicate files by MD5 digest."
+  [ ]
+
+  (let [ md5-to-filename (all-filenames-by-digest)]
+    (table
+     (map (fn [ file-rec ]
+            {:md5-digest (:md5_digest file-rec)
+             :count (:count file-rec)
+             :name (md5-to-filename (:md5_digest file-rec))})
+          (query-all (sfm/db)
+                     [(str "SELECT * FROM ("
+                           "   SELECT md5_digest, count(md5_digest) as count"
+                           "     FROM file"
+                           "    GROUP BY md5_digest)"
+                           " WHERE count > 1"
+                           " ORDER BY count")])))))
+
+(defn- cmd-describe-digest
+  "List every instance of a file with a given MD5 digest."
+  [ md5-digest ]
+  (table
+   [:catalog_name :file_name :last_modified_on :size]
+   (query-all (sfm/db)
+              [(str "SELECT file.name as file_name, catalog.name as catalog_name, file.size, file.last_modified_on"
+                    "  FROM file, catalog"
+                    " WHERE md5_digest=?"
+                    "  AND file.catalog_id=catalog.catalog_id"
+                    " ORDER BY catalog_name")
+               md5-digest])))
+
+(defn- cmd-describe-filename
+  "List every instance of a file with the given text in its filename."
+
+  [ filename-segment ]
+  (table
+   [:catalog_name :file_name :last_modified_on :size :md5_digest]
+   (query-all (sfm/db)
+              [(str "SELECT file.name as file_name, catalog.name as catalog_name, file.size, file.last_modified_on, md5_digest"
+                    "  FROM file, catalog"
+                    " WHERE INSTR(file.name, ?) > 0"
+                    "  AND file.catalog_id=catalog.catalog_id"
+                    " ORDER BY catalog_name")
+               filename-segment])))
+
+(def subcommands
+  {"digest" #'cmd-describe-digest
+   "filename" #'cmd-describe-filename
+   "duplicates" #'cmd-describe-duplicates})
