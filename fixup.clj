6d94a80a6d2528dcfda14c54b8eb284e467aefc6
Mike Schaeffer <mike@mschaef.com>
2024-03-18 15:39:16 -0400

    Add uppercase iconic to the fixup script.

--- fixup.clj
+++ fixup.clj
@@ -8,12 +8,12 @@
   (catalog/cmd-catalog-exclude-extension catalog "ithmb" "pdf" "dvi" "tsp" "xcf" "xbm" "xpm" "moi" "mom")
 
   ;; Remove an icon library, website statistics graphics, and an oracle documentation set.
-  (catalog/cmd-catalog-exclude-pattern catalog "%iconic%" "%oracle%" "%_usage%"))
+  (catalog/cmd-catalog-exclude-pattern catalog "%iconic%" "%Iconic%" "%oracle%" "%_usage%"))
 
 (doseq [ catalog (remove #(= "pictures" %) (catalog/all-catalogs))]
-  (log/info "Excluding files already in Lightroom: " catalog)
+  (log/info "Excluding files already in Lightroom:" catalog)
   (catalog/cmd-catalog-exclude-catalog catalog "pictures"))
 
 (doseq [ catalog (remove #(= "gphoto-takeout-by-year" %) (catalog/all-catalogs))]
-  (log/info "Excluding files already in Google Photos: " catalog)
+  (log/info "Excluding files already in Google Photos:" catalog)
   (catalog/cmd-catalog-exclude-catalog catalog "gphoto-takeout-by-year"))
