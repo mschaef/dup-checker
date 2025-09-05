d9a5d38046c85a56a5ee1c382eb63290d770bc4e
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2025-09-05 10:32:48 -0400

    Update dependancies.

--- project.clj
+++ project.clj
@@ -3,17 +3,19 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :plugins [[lein-ancient "1.0.0-RC3"]]
+
+  :dependencies [[org.clojure/clojure "1.12.2"]
                  [listora/again "1.0.0"]
                  [org.clj-commons/digest "1.4.100"]
-                 [software.amazon.awssdk/s3  "2.23.0"]
-                 [com.mschaef/sql-file "0.4.11"]
-                 [ring/ring-jetty-adapter "1.11.0"]
-                 [clj-http "3.12.3"
+                 [software.amazon.awssdk/s3  "2.33.3"]
+                 [com.mschaef/sql-file "0.4.13"]
+                 [ring/ring-jetty-adapter "1.14.2"]
+                 [clj-http "3.13.1"
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [org.clojure/core.async "1.6.681"]
+                 [org.clojure/core.async "1.8.741"]
                  ;; TODO: This brings in an awful lot for a CLI app.
-                 [com.mschaef/playbook "0.1.2"]]
+                 [com.mschaef/playbook "0.1.9"]]
 
   :repl-options {:init-ns dup-checker.core}
 
