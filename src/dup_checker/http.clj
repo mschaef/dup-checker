88c4f61448e487c7f2e965782fe53354c2a3ada1
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2025-09-17 18:56:09 -0400

    Add 15 second HTTP connection timeouts.

--- src/dup_checker/http.clj
+++ src/dup_checker/http.clj
@@ -6,7 +6,9 @@
 (defn- http-request* [ req-fn return-as url & {:keys [ auth ]} ]
   (let [bearer-token (if (fn? auth) (auth) auth)]
     (let [response (req-fn url
-                           (cond-> {:as return-as}
+                           (cond-> {:as return-as
+                                    :socket-timeout 15000
+                                    :connection-timeout 15000}
                              bearer-token (assoc :oauth-token bearer-token)))]
       (and (= 200 (:status response))
            (:body response)))))
